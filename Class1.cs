d31f289916a539f70e11e253e86e8fb210eb35ea
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-07 01:13:06 +0530

    [aditya]: Refactor all 3 to one generic method

--- Class1.cs
+++ Class1.cs
@@ -8,7 +8,7 @@ namespace Generics
 {
     internal class Class1
     {
-        public int MaximumInterger(int num1, int num2, int num3)
+        public T GenericsMax<T>(T num1, T num2, T num3) where T: IComparable
         {
             if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 ||
                num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 ||
@@ -33,60 +33,6 @@ namespace Generics
             throw new Exception("Num1,Num2,Num3 are same");
         }
     }
-    internal class class2
-    {
-        public float MaximumFloat(float num4, float num5, float num6)
-        {
-            if (num4.CompareTo(num5) > 0 && num4.CompareTo(num6) > 0 ||
-               num4.CompareTo(num5) > 0 && num4.CompareTo(num6) > 0 ||
-               num4.CompareTo(num5) > 0 && num4.CompareTo(num6) >= 0)
-            {
-                return num4;
-            }
-
-            if (num5.CompareTo(num4) > 0 && num5.CompareTo(num6) > 0 ||
-                num5.CompareTo(num4) >= 0 && num5.CompareTo(num6) > 0 ||
-                num5.CompareTo(num4) > 0 && num5.CompareTo(num6) >= 0)
-            {
-                return num4;
-            }
-
-            if (num6.CompareTo(num4) > 0 && num6.CompareTo(num5) > 0 ||
-                num6.CompareTo(num4) >= 0 && num6.CompareTo(num5) > 0 ||
-                num6.CompareTo(num4) > 0 && num6.CompareTo(num5) >= 0)
-            {
-                return num6;
-            }
-            throw new Exception("Num1,Num2,Num3 are same");
-        }
+   
 
     }
-    internal class class3
-    {
-        public string MaximumString(string str1, string str2, string str3)
-        {
-            if ((str1.Length).CompareTo(str2.Length) > 0 && str2.Length.CompareTo(str3.Length) > 0 ||
-               str1.Length.CompareTo(str2.Length) > 0 && str2.Length.CompareTo(str3.Length) > 0 ||
-               str1.Length.CompareTo(str2.Length) > 0 && str2.Length.CompareTo(str3.Length) >= 0)
-            {
-                return str1;
-            }
-
-            if (str2.Length.CompareTo(str1.Length) > 0 && str2.Length.CompareTo(str3.Length) > 0 ||
-                str2.Length.CompareTo(str1.Length) >= 0 && str2.Length.CompareTo(str3.Length) > 0 ||
-                str2.Length.CompareTo(str1.Length) > 0 && str2.Length.CompareTo(str3.Length) >= 0)
-            {
-                return str2;
-            }
-  
-            if (str3.Length.CompareTo(str1.Length) > 0 && str3.Length.CompareTo(str2.Length) > 0 ||
-                str3.Length.CompareTo(str1.Length) >= 0 && str3.Length.CompareTo(str2.Length) > 0 ||
-                str3.Length.CompareTo(str1.Length) > 0 && str3.Length.CompareTo(str2.Length) >= 0)
-            {
-                return str3;
-            }
-            throw new Exception("Num1,Num2,Num3 are same");
-        }
-
-    }
-}
--- Program.cs
+++ Program.cs
@@ -5,18 +5,18 @@
         static void Main(string[] args)
         {
             Class1 class1 = new Class1();
-            int value1=class1.MaximumInterger(23, 54, 89);
-            Console.WriteLine("Max integer value is : "+ value1);
+            var str=class1.GenericsMax("aditya", "waghmare", "kdjfkd");
+            Console.WriteLine("max number of string: "+str); 
+            Console.WriteLine("==========================");
 
+           var number= class1.GenericsMax(12, 23, 54);
+            Console.WriteLine("max number is: " + number);
+            Console.WriteLine("==========================");
 
-            class2 class2 = new class2();
-            float value2=class2.MaximumFloat(24.4f, 65.3f, 87.2f);
-            Console.WriteLine("Max float value is : " + value2);
-            
-            class3 class3 = new class3();
-            String st=class3.MaximumString("ad","adityatggggggggggggggg","waghmare");
-            Console.WriteLine("Maximum length of String is: " + st);
-
+           
+            var floatnum = class1.GenericsMax(23.3, 45.4, 55.3);
+            Console.WriteLine("max number of string: " + str);
+            Console.WriteLine("==========================");
         }
     }
 }

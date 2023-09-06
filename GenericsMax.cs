180da0dc618945fad454152ac66391d8eb97828c
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-07 01:58:25 +0530

    [aditya]: Refacto to create Generic class

--- GenericsMax.cs
+++ GenericsMax.cs
@@ -6,12 +6,22 @@ using System.Threading.Tasks;
 
 namespace Generics
 {
-    internal class Class1
+    public class GenericsMax<T> where T : IComparable
     {
-        public T GenericsMax<T>(T num1, T num2, T num3) where T: IComparable
+        public T num1;
+        public T num2;
+        public T num3;
+        public GenericsMax(T num1, T num2, T num3)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.num3 = num3;
+        }
+
+        public T testMaximum()
         {
             if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 ||
-               num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 ||
+               num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0 ||
                num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
             {
                 return num1;
@@ -33,6 +43,6 @@ namespace Generics
             throw new Exception("Num1,Num2,Num3 are same");
         }
     }
-   
+}
 
-    }
+    
--- Program.cs
+++ Program.cs
@@ -4,19 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Class1 class1 = new Class1();
-            var str=class1.GenericsMax("aditya", "waghmare", "kdjfkd");
-            Console.WriteLine("max number of string: "+str); 
-            Console.WriteLine("==========================");
-
-           var number= class1.GenericsMax(12, 23, 54);
-            Console.WriteLine("max number is: " + number);
-            Console.WriteLine("==========================");
-
-           
-            var floatnum = class1.GenericsMax(23.3, 45.4, 55.3);
+            GenericsMax<int> genericsMax = new GenericsMax<int>(40, 20, 53);
+            var str = genericsMax.testMaximum();
             Console.WriteLine("max number of string: " + str);
             Console.WriteLine("==========================");
+
+
+        }
         }
     }
-}
